1bfff160e63d0191900383083e011d65390e6e8f
JerryUusis <jerry.uusitalo@gmail.com>
2023-11-28 14:30:55 +0200

    Refactor functions

--- src/components/NewGame.jsx
+++ src/components/NewGame.jsx
@@ -1,12 +1,19 @@
-const NewGame = ({onClick, setPlayerName}) => {
+import { useState } from "react"
+const NewGame = ({ onClick }) => {
+    const [name, setName] = useState("");
+
+    const setPlayerName = (event) => {
+        setName(event.target.value)
+    }
+
     return (
         <div>
             <h2>Aloita uusi peli  kirjoittamalla nimesi ja valitsemalla vaikeusaste</h2>
-            <input type="text" name="" id="" placeholder="Kirjoita nimesi" onChange={setPlayerName}/>
+            <input type="text" placeholder="Kirjoita nimesi" onChange={setPlayerName} />
             <div className="difficulty-button-container" >
-                <button onClick={() => onClick("easy")}>Helppo</button>
-                <button onClick={() => onClick("medium")}>Keskitaso</button>
-                <button onClick={() => onClick("hard")}>Vaikea</button>
+                <button onClick={() => onClick("easy", name)}>Helppo</button>
+                <button onClick={() => onClick("medium", name)}>Keskitaso</button>
+                <button onClick={() => onClick("hard", name)}>Vaikea</button>
             </div>
         </div>
     )
