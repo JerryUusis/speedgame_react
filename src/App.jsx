b2688a0bb4fa3cc1d51b40f45b5b826bd205a8ac
JerryUusis <jerry.uusitalo@gmail.com>
2023-11-28 11:32:39 +0200

    Add functions for buttons and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import NewGame from "./components/NewGame"
+import { useState } from "react"
+
 function App() {
+  const [name, setName] = useState("");
+
+  const gameSetHandler = difficulty => {
+    console.log(difficulty)
+  }
+
+  const setPlayerName = (event) => {
+    return setName(event.target.value, name)
+  }
+
+  console.log(name)
+
 
   return (
     <>
+      <h1>Nopeustesti</h1>
+      <NewGame 
+      onClick={gameSetHandler}
+      setPlayerName={setPlayerName}
+       />
     </>
   )
 }
