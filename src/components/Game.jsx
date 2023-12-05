ff7dd0cefb33c8c6aa342dd666b44c669b5a7d07
JerryUusis <jerry.uusitalo@gmail.com>
2023-12-05 13:15:07 +0200

    Make mapping more readible and pass current on Circle

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,16 @@
 import Circle from "../ui_components/Circle";
 
-const Game = ({ score, circles, stopHandler, circleClick }) => {
+const Game = ({ score, circles, stopHandler, circleClick, current }) => {
 
     return (
         <div className="page-content-container">
             <div className="circle-container">
-            {circles.map((_, i) => <Circle key={i} circleClick={circleClick} id={i}/>)}
+            {circles.map((_, i) => 
+            <Circle 
+            key={i} 
+            circleClick={circleClick} 
+            id={i} 
+            current={current === i}/>)}
             </div>
             <h2>Current score</h2>
             <p>{score}</p>
