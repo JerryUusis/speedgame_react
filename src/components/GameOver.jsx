feb2a4fdcaf7422003c839cddbbb007188d0b5a2
JerryUusis <jerry.uusitalo@gmail.com>
2023-12-05 12:39:39 +0200

    Add placeholder results table

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,11 +1,26 @@
-const GameOver = ({ closeHandler, name, difficulty }) => {
+const GameOver = ({ closeHandler, name, difficulty, score }) => {
     return (
 
         <div className="page-content-container">
             <h2>Peli päättyi</h2>
             <p>Hei {name}</p>
-            <p>Pelasit vaikeusasteella {difficulty}</p>
+            <p>Pelasit vaikeusasteella: {difficulty}</p>
+            <p>Pisteesi: {score}</p>
             <button onClick={closeHandler}>Sulje</button>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Nimi</th>
+                        <th>Pisteet</th>
+                        <th>Vaikeusaste</th>
+                    </tr>
+                    <tr>
+                        <td>{name}</td>
+                        <td>{score}</td>
+                        <td>{difficulty}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
